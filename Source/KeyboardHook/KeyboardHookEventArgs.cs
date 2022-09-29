238866e105841a16e0708ad81cdcfa5262243f29
Turbék György <gyorgy.turbek@gmail.com>
2022-09-29 15:01:17 +0200

    Added low level keyboard hook

---
+++ Source/KeyboardHook/KeyboardHookEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Source.KeyboardHook;
+
+public sealed class KeyboardHookEventArgs : HandledEventArgs
+{
+    public KeyboardState KeyboardState { get; }
+    public KeycodeInterpreter KeyboardData { get; }
+
+    public KeyboardHookEventArgs(KeycodeInterpreter keyboardData, KeyboardState keyboardState)
+    {
+        KeyboardData = keyboardData;
+        KeyboardState = keyboardState;
+    }
+}
---
+++ Source/KeyboardHook/KeyboardState.cs
@@ -0,0 +1,9 @@
+﻿namespace Source.KeyboardHook;
+
+public enum KeyboardState
+{
+    KeyDown = 256,
+    KeyUp = 257,
+    SysKeyDown = 260,
+    SysKeyUp = 261
+}
